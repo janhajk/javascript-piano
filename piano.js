3349127422a86d884c83aada519197a78806b9f4
Jan <janhajk@gmail.com>
2017-08-16 19:57:14 +0000

    bug

--- piano.js
+++ piano.js
@@ -9,14 +9,14 @@
    var tunesCount = 0;
 
    var getNameFromKey = function(key) {
-      var index = (key-(Math.floor(key / 13)*13))-(key/13 >= 2?0:1);
-      index = index < 0 && key/13 >= 2 ? 0 : index;
+      var index = (key-(Math.floor(key / 12)*12))-(key/12 >= 2?0:1);
+      index = index < 0 && key/12 >= 2 ? 0 : index;
       return tones[index];
    };
 
    var getHertzFromKey = function(key) {
       var A = 440; // Hz
-      return Math.pow(Math.pow(2, 1/12),key-14) * A;
+      return Math.pow(Math.pow(2, 1/12),key-13) * A;
    };
 
    var Key = function(key, xPos, yPos) {
@@ -66,7 +66,7 @@
       var count = 88;
       var key = null;
       for (let i = 0; i < count; i++) {
-         key = new Key(i+14, i*width, 0);
+         key = new Key(i+13, i*width, 0);
          key.append(divPiano);
       }
    });
